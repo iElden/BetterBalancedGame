ff086f88eb3139b67efb5677134b75389ed6ddc3
Elden <mceldenn@gmail.com>
2020-11-22 18:46:58 +0100

    Added Babylon nerf (100% => 85% for eureka)

--- sql/new_bbg_nfp_babylon.sql
+++ sql/new_bbg_nfp_babylon.sql
@@ -1,19 +1,5 @@
--- Babylon Pack - Great Writer
+--==============================================================
+--******			C I V I L I Z A T I O N S			  ******
+--==============================================================
 
--- Babylon Pack - Great Artist
-UPDATE GreatWork_YieldChanges SET YieldChange=4 WHERE GreatWorkType='GREATWORK_BEHZAD_1';
-UPDATE GreatWork_YieldChanges SET YieldChange=4 WHERE GreatWorkType='GREATWORK_BEHZAD_1';
-UPDATE GreatWork_YieldChanges SET YieldChange=4 WHERE GreatWorkType='GREATWORK_BEHZAD_2';
-UPDATE GreatWork_YieldChanges SET YieldChange=4 WHERE GreatWorkType='GREATWORK_TOHAKU_1';
-UPDATE GreatWork_YieldChanges SET YieldChange=4 WHERE GreatWorkType='GREATWORK_TOHAKU_2';
-UPDATE GreatWork_YieldChanges SET YieldChange=4 WHERE GreatWorkType='GREATWORK_TOHAKU_3';
-UPDATE GreatWork_YieldChanges SET YieldChange=4 WHERE GreatWorkType='GREATWORK_KANDINSKY_1';
-UPDATE GreatWork_YieldChanges SET YieldChange=4 WHERE GreatWorkType='GREATWORK_KANDINSKY_2';
-UPDATE GreatWork_YieldChanges SET YieldChange=4 WHERE GreatWorkType='GREATWORK_KANDINSKY_3';
--- Babylon Pack - Great Musician
-UPDATE GreatWork_YieldChanges SET YieldChange=12 WHERE GreatWorkType='GREATWORK_BABYLON_CANTEMIR_1';
-UPDATE GreatWork_YieldChanges SET YieldChange=12 WHERE GreatWorkType='GREATWORK_BABYLON_CANTEMIR_2';
-UPDATE GreatWork_YieldChanges SET YieldChange=12 WHERE GreatWorkType='GREATWORK_BABYLON_CANTEMIR_3';
-UPDATE GreatWork_YieldChanges SET YieldChange=12 WHERE GreatWorkType='GREATWORK_BABYLON_JOPLIN_1';
-UPDATE GreatWork_YieldChanges SET YieldChange=12 WHERE GreatWorkType='GREATWORK_BABYLON_JOPLIN_2';
-UPDATE GreatWork_YieldChanges SET YieldChange=12 WHERE GreatWorkType='GREATWORK_BABYLON_JOPLIN_3';
+UPDATE ModifierArguments SET Value='85' WHERE ModifierId=TRAIT_EUREKA_INCREASE;
