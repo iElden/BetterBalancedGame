9e3febe40bcec16b1bb0d2014e959caf79bdf76d
Elden <mceldenn@gmail.com>
2020-11-22 18:25:49 +0100

    Added Carthage and Ngazargamu city state nerf

---
+++ sql/new_bbg_citystates.sql
@@ -0,0 +1,8 @@
+--==============================================================
+--******			       CITY STATES      			  ******
+--==============================================================
+
+-- Ngazargamu legacy city-state is carthage
+UPDATE ModifierArguments SET Value='10' WHERE MINOR_CIV_CARTHAGE_BARRACKS_STABLE_PURCHASE_BONUS;
+UPDATE ModifierArguments SET Value='10' WHERE MINOR_CIV_CARTHAGE_ARMORY_PURCHASE_BONUS;
+UPDATE ModifierArguments SET Value='10' WHERE MINOR_CIV_CARTHAGE_MILITARY_ACADEMY_PURCHASE_BONUS;
---
+++ sql/new_bbg_citystates_xp2.sql
@@ -0,0 +1,8 @@
+--==============================================================
+--******			       CITY STATES      			  ******
+--==============================================================
+
+-- Ngazargamu give 10% reduction insead on 20% reduction
+UPDATE ModifierArguments SET Value='10' WHERE MINOR_CIV_NGAZARGAMU_BARRACKS_STABLE_PURCHASE_BONUS;
+UPDATE ModifierArguments SET Value='10' WHERE MINOR_CIV_NGAZARGAMU_ARMORY_PURCHASE_BONUS;
+UPDATE ModifierArguments SET Value='10' WHERE MINOR_CIV_NGAZARGAMU_MILITARY_ACADEMY_PURCHASE_BONUS;
