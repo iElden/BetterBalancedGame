1341696ff03f3748e3227e16e838409ad96e56cb
Elden <mceldenn@gmail.com>
2020-11-27 13:08:49 +0100

    Added Hungary loc

--- sql/new_bbg_citystates.sql
+++ sql/new_bbg_citystates.sql
@@ -3,6 +3,6 @@
 --==============================================================
 
 -- Ngazargamu legacy city-state is carthage
-UPDATE ModifierArguments SET Value='10' WHERE ModifierId=MINOR_CIV_CARTHAGE_BARRACKS_STABLE_PURCHASE_BONUS;
-UPDATE ModifierArguments SET Value='10' WHERE ModifierId=MINOR_CIV_CARTHAGE_ARMORY_PURCHASE_BONUS;
-UPDATE ModifierArguments SET Value='10' WHERE ModifierId=MINOR_CIV_CARTHAGE_MILITARY_ACADEMY_PURCHASE_BONUS;
+UPDATE ModifierArguments SET Value='10' WHERE ModifierId='MINOR_CIV_CARTHAGE_BARRACKS_STABLE_PURCHASE_BONUS';
+UPDATE ModifierArguments SET Value='10' WHERE ModifierId='MINOR_CIV_CARTHAGE_ARMORY_PURCHASE_BONUS';
+UPDATE ModifierArguments SET Value='10' WHERE ModifierId='MINOR_CIV_CARTHAGE_MILITARY_ACADEMY_PURCHASE_BONUS';
--- sql/new_bbg_nfp_babylon.sql
+++ sql/new_bbg_nfp_babylon.sql
@@ -2,4 +2,12 @@
 --******			C I V I L I Z A T I O N S			  ******
 --==============================================================
 
-UPDATE ModifierArguments SET Value='85' WHERE ModifierId=TRAIT_EUREKA_INCREASE;
+INSERT INTO Modifiers(ModifierId, ModifierType) VALUES
+    ('BBG_TRAIT_BABYLON_EUREKA', 'MODIFIER_PLAYER_ADJUST_TECHNOLOGY_BOOST');
+
+INSERT INTO ModifierArguments(ModifierId, Name, Value, Extra) VALUES
+    ('BBG_TRAIT_BABYLON_EUREKA', 'Amount', '45', '-1');
+
+DELETE FROM TraitModifiers WHERE TraitType='TRAIT_CIVILIZATION_BABYLON' AND ModifierID='TRAIT_EUREKA_INCREASE';
+INSERT INTO TraitModifiers(TraitType, ModifierID) VALUES
+    ('TRAIT_CIVILIZATION_BABYLON', 'BBG_TRAIT_BABYLON_EUREKA');
