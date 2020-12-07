b4b70f0d83e598460a65120d6774f107b23844e2
Elden <mceldenn@gmail.com>
2020-12-07 16:13:12 +0100

    build 11 - Russian + korean LOC and fix Imhotep

--- sql/new_bbg_great_people.sql
+++ sql/new_bbg_great_people.sql
@@ -1,2 +1,2 @@
 -- Reduce Imhotep to 1 charge
-UPDATE FROM GreatPersonIndividuals SET ActionCharges=1 WHERE GreatPersonIndividualType='GREAT_PERSON_INDIVIDUAL_IMHOTEP';
+UPDATE GreatPersonIndividuals SET ActionCharges=1 WHERE GreatPersonIndividualType='GREAT_PERSON_INDIVIDUAL_IMHOTEP';
