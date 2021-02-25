78117e191dd144949862dee9382ce549afb0a102
Elden <mceldenn@gmail.com>
2021-02-25 18:59:50 +0100

    Vietnam nerf

---
+++ sql/new_bbg_nfp_vietnam.sql
@@ -0,0 +1,3 @@
+
+UPDATE ModifierArguments SET Value='4' WHERE ModifierId='TRIEU_FRIENDLY_COMBAT' AND Name='Amount';
+UPDATE ModifierArguments SET Value='2' WHERE ModifierId='TRIEU_UNFRIENDLY_COMBAT' AND Name='Amount';
