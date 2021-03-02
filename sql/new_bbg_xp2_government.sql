3b5b781c38834c59a6c9c76fa7d75e9fb8039221
Elden <mceldenn@gmail.com>
2021-03-02 15:03:05 +0100

    Modified monarchy and modinfo

---
+++ sql/new_bbg_xp2_government.sql
@@ -0,0 +1,14 @@
+--
+-- Government xp2
+--
+
+-- Replace +2 favor on renaissance wall with monarchy to +2 culture
+UPDATE Modifiers SET ModifierType='MODIFIER_PLAYER_CITIES_ADJUST_BUILDING_YIELD_CHANGE' WHERE ModifierId='MONARCHY_STARFORT_FAVOR';
+UPDATE Modifiers SET SubjectRequirementSetId=NULL WHERE ModifierId='MONARCHY_STARFORT_FAVOR';
+
+-- Add +2 culture to renaissance wall
+DELETE FROM ModifierArguments WHERE ModifierId='MONARCHY_STARFORT_FAVOR';
+INSERT INTO (ModifierId, Name, Value) VALUES
+    ('MONARCHY_STARFORT_FAVOR', 'BuildingType', 'BUILDING_STAR_FORT')
+    ('MONARCHY_STARFORT_FAVOR', 'YieldType', 'YIELD_CULTURE'),
+    ('MONARCHY_STARFORT_FAVOR', 'Amount', '2');
