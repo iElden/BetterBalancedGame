674c61a6da2c680c92029681abbee943d710e4d5
Elden <mceldenn@gmail.com>
2021-03-02 17:01:06 +0100

    4.3.0 (#91)
    * merged 1
    
    * Fixed ParameterId display
    
    * Flag To Win Domination
    
    * merged 2
    
    * Fix for rome free building
    
    * polishing
    
    * merged 3
    
    * tEXT UPDATED
    
    * Try and failed to get the score setting working
    
    * Made Conquest and Domination on two separated settings
    
    * merged 4
    
    * merged 5
    
    * Added Balanced Barbarian Option
    
    * Merged beta In current master
    
    * Deleted duplicate campus bonus of Gear barrier reef
    
    * Deleted 2nd title on audience chamber
    
    * Modified monarchy and modinfo
    
    * Nerf Vietnam UD and UU
    
    * nerf russia and canada
    
    * fix +2 culture on rennaissance wall
    
    * Updated text for audience and canada
    
    * Languages update vietnam + monarchy
    
    * fr and kr language update 4.2.4
    
    Co-authored-by: D. / Jack The Narrator <70652395+d-jackthenarrator@users.noreply.github.com>

---
+++ config/config.sql
@@ -0,0 +1,10 @@
+------------------------------------------------------------------------------
+--	FILE:	 config.sql
+--	AUTHOR:  iElden, D. / Jack The Narrator
+--	PURPOSE: Config Database adjustment for front-end
+------------------------------------------------------------------------------
+--==============================================================================================
+--******								ADDITIONAL CONFIG								******
+--==============================================================================================
+
+UPDATE Parameters SET Visible=0 WHERE Key2 IN ('RULESET_STANDARD', 'RULESET_EXPANSION_1', 'RULESET_EXPANSION_2') AND ConfigurationId='GAME_NO_BARBARIANS';
--- sql/new_bbg_nfp_vietnam.sql
+++ sql/new_bbg_nfp_vietnam.sql
@@ -1,3 +1,7 @@
 
 UPDATE ModifierArguments SET Value='4' WHERE ModifierId='TRIEU_FRIENDLY_COMBAT' AND Name='Amount';
 UPDATE ModifierArguments SET Value='2' WHERE ModifierId='TRIEU_UNFRIENDLY_COMBAT' AND Name='Amount';
+
+UPDATE District_Adjacencies SET YieldChangeId='District_Culture_Standard' WHERE DistrictType='DISTRICT_THANH';
+
+UPDATE Units SET BaseMoves=2 WHERE UnitType='UNIT_VIETNAMESE_VOI_CHIEN';
