2114c6df256166bd8e82ed30d30514d123680e6b
Elden <mceldenn@gmail.com>
2021-03-26 17:06:33 +0100

    Spain +25% production toward Harbor building

--- sql/new_bbg_spain.sql
+++ sql/new_bbg_spain.sql
@@ -1,8 +1,6 @@
 -- Author: iElden
 -- Making this is like spain but s is silent
 
-
-
 INSERT INTO Modifiers(ModifierId, ModifierType, OwnerRequirementSetId) VALUES
     ('BBG_SPAIN_FLEET_DISCOUNT', 'MODIFIER_CITY_CORPS_ARMY_ADJUST_DISCOUNT', 'BBG_PLAYER_IS_SPAIN');
 
@@ -13,9 +11,16 @@ INSERT INTO ModifierArguments(ModifierId, Name, Value) VALUES
 INSERT INTO BuildingModifiers(BuildingType, ModifierId) VALUES
     ('BUILDING_SHIPYARD', 'BBG_SPAIN_FLEET_DISCOUNT');
 
+--
+INSERT OR IGNORE INTO TraitModifiers (TraitType , ModifierId) VALUES
+	('TRAIT_CIVILIZATION_TREASURE_FLEET' , 'BBG_SPAIN_BUILDING_PROD_BOOST');
+INSERT OR IGNORE INTO Modifiers (ModifierId , ModifierType , SubjectRequirementSetId , OwnerRequirementSetId) VALUES
+	('BBG_SPAIN_BUILDING_PROD_BOOST' , 'MODIFIER_PLAYER_CITIES_ADJUST_BUILDING_PRODUCTION' , NULL , NULL);
+INSERT OR IGNORE INTO ModifierArguments (ModifierId , Name , Value) VALUES
+	('BBG_SPAIN_BUILDING_PROD_BOOST' , 'DistrictType' , 'DISTRICT_HARBOR'),
+	('BBG_SPAIN_BUILDING_PROD_BOOST' , 'Amount'       , '25');
 
 -- Leader is Phillipe Requirement
-
 INSERT OR IGNORE INTO RequirementSets(RequirementSetId , RequirementSetType) VALUES
 	('BBG_PLAYER_IS_SPAIN', 'REQUIREMENTSET_TEST_ANY');
 
