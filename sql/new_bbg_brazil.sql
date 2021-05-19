5e1a8a3854610673cb9233d3b01018b0c20ef3fe
Elden <mceldenn@gmail.com>
2021-05-19 02:42:19 +0200

    Brazil UD don't require population

--- sql/new_bbg_brazil.sql
+++ sql/new_bbg_brazil.sql
@@ -1,4 +1,7 @@
 -- Author: iElden
 
+-- Carnivals don't require population
+UPDATE Districts SET RequiresPopulation=0 WHERE DistrictType IN ('DISTRICT_STREET_CARNIVAL', 'DISTRICT_WATER_STREET_CARNIVAL');
+
 -- Up Minas by +5/+5 to match battleship up
 UPDATE Units SET Combat=75, RangedCombat=85 WHERE UnitType='UNIT_BRAZILIAN_MINAS_GERAES';
