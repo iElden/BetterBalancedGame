88bf1f5c21feebee8baea4ff0c1079bd4de8830b
D. / Jack The Narrator <70652395+d-jackthenarrator@users.noreply.github.com>
2021-05-22 16:52:47 +0100

    SV fixed
    adjusted the sql so it is only for GS style victory. (We balance assuming GS anyway)
    Adjusted the lua so it is now a simple passthrough
    Adjusted trhe SV style based on Chuck's feedback: so the distance to travel is bigger and the time is speed up at tech level now.

--- sql/victories/new_bbg_sv_1.sql
+++ sql/victories/new_bbg_sv_1.sql
@@ -7,6 +7,13 @@
 --******				SV: FAST SETTINGS 											 ******
 --==============================================================================================
 
-UPDATE GlobalParameters SET Value='1' WHERE Name='SCIENCE_VICTORY_POINTS_REQUIRED';
+UPDATE GlobalParameters SET Value='45' WHERE Name='SCIENCE_VICTORY_POINTS_REQUIRED';
 UPDATE Projects SET Cost='1500' WHERE ProjectType='PROJECT_LAUNCH_EXOPLANET_EXPEDITION';
 UPDATE Projects SET Cost='1500' WHERE ProjectType='PROJECT_LAUNCH_MARS_BASE';
+
+UPDATE Technologies SET Cost='1700' WHERE TechnologyType='TECH_SEASTEADS';
+UPDATE Technologies SET Cost='1700' WHERE TechnologyType='TECH_ADVANCED_AI';
+UPDATE Technologies SET Cost='1700' WHERE TechnologyType='TECH_ADVANCED_POWER_CELLS';
+UPDATE Technologies SET Cost='1700' WHERE TechnologyType='TECH_CYBERNETICS';
+UPDATE Technologies SET Cost='1700' WHERE TechnologyType='TECH_PREDICTIVE_SYSTEMS';
+UPDATE Technologies SET Cost='2000' WHERE TechnologyType='TECH_OFFWORLD_MISSION';
