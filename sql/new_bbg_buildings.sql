8c20ba33ab65e5b640de9747bae838b15d0b091e
Elden <mceldenn@gmail.com>
2021-09-06 01:50:33 +0200

    Removed ability for preserve to plant wood

--- sql/new_bbg_buildings.sql
+++ sql/new_bbg_buildings.sql
@@ -12,13 +12,13 @@ INSERT INTO District_TradeRouteYields(DistrictType, YieldType, YieldChangeAsOrig
 UPDATE Districts SET Cost=54 WHERE DistrictType IN ('DISTRICT_CANAL', 'DISTRICT_DAM');
 
 -- Preserve give ability to plant wood.
-INSERT INTO Modifiers(ModifierId, ModifierType) VALUES
-    ('BBG_PRESERVE_PLANT_WOOD', 'MODIFIER_PLAYER_ADJUST_FEATURE_UNLOCK');
-INSERT INTO ModifierArguments(ModifierId, Name, Value) VALUES
-    ('BBG_PRESERVE_PLANT_WOOD', 'FeatureType', 'FEATURE_FOREST'),
-    ('BBG_PRESERVE_PLANT_WOOD', 'CivicType', 'CIVIC_CODE_OF_LAWS');
-INSERT INTO DistrictModifiers(DistrictType, ModifierId) VALUES
-    ('DISTRICT_PRESERVE', 'BBG_PRESERVE_PLANT_WOOD');
+-- INSERT INTO Modifiers(ModifierId, ModifierType) VALUES
+--    ('BBG_PRESERVE_PLANT_WOOD', 'MODIFIER_PLAYER_ADJUST_FEATURE_UNLOCK');
+-- INSERT INTO ModifierArguments(ModifierId, Name, Value) VALUES
+--    ('BBG_PRESERVE_PLANT_WOOD', 'FeatureType', 'FEATURE_FOREST'),
+--    ('BBG_PRESERVE_PLANT_WOOD', 'CivicType', 'CIVIC_CODE_OF_LAWS');
+-- INSERT INTO DistrictModifiers(DistrictType, ModifierId) VALUES
+--    ('DISTRICT_PRESERVE', 'BBG_PRESERVE_PLANT_WOOD');
 
 -- ==============
 -- == BUILDING ==
