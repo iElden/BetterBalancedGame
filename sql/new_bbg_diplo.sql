5813893ee23671680980e64ab14209b95c7292f2
Elden <mceldenn@gmail.com>
2021-10-28 17:34:34 +0200

    Revert "DV resolution to -3"
    This reverts commit a4fdff6f599f4f7bac4d7b5c026efe02e65731a8.

--- sql/new_bbg_diplo.sql
+++ sql/new_bbg_diplo.sql
@@ -2,6 +2,3 @@
 
 -- Each City-state suz give 2 diplomatic favor.
 UPDATE GlobalParameters SET Value=2 WHERE Name='WORLD_CONGRESS_SUZERAIN_FAVOR_PER_TURN';
-
--- DV Resolution to -3 points (from -2)
-UPDATE ModifierArguments SET Value='-3' WHERE ModifierId='SUBTRACT_DIPLOMATIC_VICTORY_POINTS' AND Name='Amount';
