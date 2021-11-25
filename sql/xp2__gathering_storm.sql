bf5077301b8abc2e37995e5a0c211a1741a148a0
Elden <mceldenn@gmail.com>
2021-11-25 20:25:08 +0100

    Added Eleonore big buff

--- sql/xp2__gathering_storm.sql
+++ sql/xp2__gathering_storm.sql
@@ -107,17 +107,64 @@ INSERT OR IGNORE INTO RequirementSetRequirements (RequirementSetId, RequirementI
 --==========
 -- ELEANOR
 --==========
-INSERT OR IGNORE INTO TraitModifiers (TraitType, ModifierId)
-	VALUES
+INSERT OR IGNORE INTO TraitModifiers (TraitType, ModifierId) VALUES
 	('TRAIT_LEADER_ELEANOR_LOYALTY', 'THEATER_BUILDING_PRODUCTION_BONUS_CPLMOD');
-INSERT OR IGNORE INTO Modifiers (ModifierId, ModifierType, SubjectRequirementSetId, OwnerRequirementSetId)
-	VALUES
+INSERT OR IGNORE INTO Modifiers (ModifierId, ModifierType, SubjectRequirementSetId, OwnerRequirementSetId) VALUES
 	('THEATER_BUILDING_PRODUCTION_BONUS_CPLMOD', 'MODIFIER_PLAYER_CITIES_ADJUST_BUILDING_PRODUCTION', NULL, NULL);
-INSERT OR IGNORE INTO ModifierArguments (ModifierId, Name, Value)
-	VALUES
+INSERT OR IGNORE INTO ModifierArguments (ModifierId, Name, Value) VALUES
 	('THEATER_BUILDING_PRODUCTION_BONUS_CPLMOD', 'DistrictType', 'DISTRICT_THEATER'),
-	('THEATER_BUILDING_PRODUCTION_BONUS_CPLMOD', 'Amount', '100'                     );
+	('THEATER_BUILDING_PRODUCTION_BONUS_CPLMOD', 'Amount', '100');
 
+CREATE TABLE TmpEldenEleonore(DistrictType PRIMARY KEY NOT NULL, YieldType NOT NULL);
+INSERT INTO TmpEldenEleonore(DistrictType, YieldType) VALUES
+    ('DISTRICT_NEIGHBORHOOD', 'YIELD_FOOD'),
+    ('DISTRICT_INDUSTRIAL_ZONE', 'YIELD_PRODUCTION'),
+    ('DISTRICT_COMMERCIAL_HUB', 'YIELD_GOLD'),
+    ('DISTRICT_HARBOR', 'YIELD_GOLD'),
+    ('DISTRICT_CAMPUS', 'YIELD_SCIENCE'),
+    ('DISTRICT_THEATER', 'YIELD_CULTURE'),
+    ('DISTRICT_HOLY_SITE', 'YIELD_FAITH');
+
+-- Create and attach modifier to Eleanor
+INSERT INTO TraitModifiers(TraitType, ModifierId)
+    SELECT 'TRAIT_LEADER_ELEANOR_LOYALTY', 'BBG_ELEONORE_' || GreatWorkObjectTypes.GreatWorkObjectType || '_' || DistrictType
+    FROM TmpEldenEleonore CROSS JOIN GreatWorkObjectTypes;
+INSERT INTO Modifiers(ModifierId, ModifierType)
+    SELECT 'BBG_ELEONORE_' || GreatWorkObjectTypes.GreatWorkObjectType || '_' || DistrictType, 'MODIFIER_PLAYER_CITIES_ATTACH_MODIFIER'
+    FROM TmpEldenEleonore CROSS JOIN GreatWorkObjectTypes;
+INSERT INTO Modifiers(ModifierId, ModifierType, SubjectRequirementSetId)
+    SELECT 'BBG_ELEONORE_' || GreatWorkObjectTypes.GreatWorkObjectType || '_' || DistrictType || '_MODIFIER', 'MODIFIER_SINGLE_CITY_ADJUST_GREATWORK_YIELD', 'BBG_CITY_HAS_' || DistrictType
+    FROM TmpEldenEleonore CROSS JOIN GreatWorkObjectTypes;
+
+-- Create District Requirements
+INSERT INTO RequirementSets(RequirementSetId , RequirementSetType)
+    SELECT 'BBG_CITY_HAS_' || DistrictType, 'REQUIREMENTSET_TEST_ALL'
+    FROM TmpEldenEleonore;
+INSERT INTO RequirementSetRequirements(RequirementSetId , RequirementId)
+    SELECT 'BBG_CITY_HAS_' || DistrictType, 'BBG_CITY_HAS_' || DistrictType || '_REQUIREMENT'
+    FROM TmpEldenEleonore;
+INSERT INTO Requirements(RequirementId , RequirementType)
+    SELECT 'BBG_CITY_HAS_' || DistrictType || '_REQUIREMENT', 'REQUIREMENT_CITY_HAS_DISTRICT'
+    FROM TmpEldenEleonore;
+INSERT INTO RequirementArguments(RequirementId , Name, Value)
+    SELECT 'BBG_CITY_HAS_' || DistrictType || '_REQUIREMENT', 'DistrictType', DistrictType
+    FROM TmpEldenEleonore;
+
+-- Set Modifiers Arguments to correct value
+INSERT INTO ModifierArguments(ModifierId, Name, Value)
+    SELECT 'BBG_ELEONORE_' || GreatWorkObjectTypes.GreatWorkObjectType || '_' || DistrictType, 'ModifierId', 'BBG_ELEONORE_' || GreatWorkObjectTypes.GreatWorkObjectType || '_' || DistrictType || '_MODIFIER'
+    FROM TmpEldenEleonore CROSS JOIN GreatWorkObjectTypes;
+INSERT INTO ModifierArguments(ModifierId, Name, Value)
+    SELECT 'BBG_ELEONORE_' || GreatWorkObjectTypes.GreatWorkObjectType || '_' || DistrictType || '_MODIFIER', 'GreatWorkObjectType', GreatWorkObjectType
+    FROM TmpEldenEleonore CROSS JOIN GreatWorkObjectTypes;
+INSERT INTO ModifierArguments(ModifierId, Name, Value)
+    SELECT 'BBG_ELEONORE_' || GreatWorkObjectTypes.GreatWorkObjectType || '_' || DistrictType || '_MODIFIER', 'YieldType', YieldType
+    FROM TmpEldenEleonore CROSS JOIN GreatWorkObjectTypes;
+INSERT INTO ModifierArguments(ModifierId, Name, Value)
+    SELECT 'BBG_ELEONORE_' || GreatWorkObjectTypes.GreatWorkObjectType || '_' || DistrictType || '_MODIFIER', 'YieldChange', '2'
+    FROM TmpEldenEleonore CROSS JOIN GreatWorkObjectTypes;
+
+DROP TABLE TmpEldenEleonore;
 
 --==========
 -- FRANCE
