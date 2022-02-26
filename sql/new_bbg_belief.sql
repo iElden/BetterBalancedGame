9a562d35cf6c3333320d8da9246cfb9447d1e237
Elden <mceldenn@gmail.com>
2022-02-26 14:10:38 +0100

    Synagogue to 7 faith

--- sql/new_bbg_belief.sql
+++ sql/new_bbg_belief.sql
@@ -68,3 +68,6 @@ UPDATE ModifierArguments SET Value='30' WHERE ModifierId='GOD_OF_THE_FORGE_UNIT_
 UPDATE ModifierArguments SET Value='3' WHERE ModifierId='DEFENDER_OF_FAITH_COMBAT_BONUS_MODIFIER' AND Name='Amount';
 -- Crusade nerf to +5
 UPDATE ModifierArguments SET Value='5' WHERE ModifierId='JUST_WAR_COMBAT_BONUS_MODIFIER' AND Name='Amount';
+
+-- Synagogue to 7 Faith:
+UPDATE Building_YieldChanges SET YieldChange=7 WHERE BuildingType='BUILDING_SYNAGOGUE' AND YieldType='YIELD_FAITH';
