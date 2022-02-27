73bc442fda14822440d0055113ed34e780956418
Elden <mceldenn@gmail.com>
2022-02-27 16:28:45 +0100

    Scythian horse cost 10 ns

--- sql/xp2__gathering_storm.sql
+++ sql/xp2__gathering_storm.sql
@@ -296,15 +296,13 @@ UPDATE Units_XP2 SET ResourceCost=20 WHERE UnitType='UNIT_PERSIAN_IMMORTAL';
 UPDATE Units_XP2 SET ResourceCost=20 WHERE UnitType='UNIT_ROMAN_LEGION';
 
 
-
 --==================
--- Sumeria
+-- Scythia
 --==================
-/* REVERT TO BASE GAME
-INSERT OR IGNORE INTO Units_XP2 (UnitType, ResourceCost) VALUES
-	('UNIT_SUMERIAN_WAR_CART', 10);
-
-*/
+-- Scythian horse cost 5 online speed
+INSERT INTO Units_XP2(UnitType, ResourceCost) VALUES
+	('UNIT_SCYTHIAN_HORSE_ARCHER', 10);
+UPDATE Units SET StrategicResource='RESOURCE_HORSES' WHERE UnitType='UNIT_SCYTHIAN_HORSE_ARCHER';
 
 --==================
 -- Sweden
