26d1a73de496860668de2da733c82c9123befa08
Tim McGee <temcgee7@gmail.com>
2022-04-18 11:41:59 -0400

    moving from goergia to religious unity

--- sql/base.sql
+++ sql/base.sql
@@ -908,6 +908,8 @@ INSERT OR IGNORE INTO RequirementSetRequirements (RequirementSetId , Requirement
     VALUES ('CITY_FOLLOWS_RELIGION_HAS_HOLY_SITE' , 'REQUIRES_CITY_FOLLOWS_RELIGION');
 INSERT OR IGNORE INTO RequirementSetRequirements (RequirementSetId , RequirementId)
 	VALUES ('CITY_FOLLOWS_RELIGION_HAS_HOLY_SITE' , 'REQUIRES_CITY_HAS_HOLY_SITE');
+-- Religious Unity gives 2 envoys instead of 1
+update ModifierArguments set value = 2 where ModifierId = 'RELIGIOUS_UNITY_ENVOY_ON_ADOPTION' and name = 'Amount';
 -- Warrior Monks +5 Combat Strength
 -- 23/04/2021: Implemented by Firaxis
 -- UPDATE Units SET Combat=40 WHERE UnitType='UNIT_WARRIOR_MONK';
