6e3f8450909fd307d49ecfaed1672ab1ef667ccf
Elden <mceldenn@gmail.com>
2022-06-28 00:41:44 +0200

    Spain Removed harbor building prod bonus

--- sql/new_bbg_spain.sql
+++ sql/new_bbg_spain.sql
@@ -47,15 +47,6 @@ INSERT INTO ModifierArguments(ModifierId, Name, Value) VALUES
 INSERT INTO BuildingModifiers(BuildingType, ModifierId) VALUES
     ('BUILDING_SHIPYARD', 'BBG_SPAIN_FLEET_DISCOUNT');
 
--- +25% production toward harbour building.
-INSERT OR IGNORE INTO TraitModifiers (TraitType , ModifierId) VALUES
-	('TRAIT_CIVILIZATION_TREASURE_FLEET' , 'BBG_SPAIN_BUILDING_PROD_BOOST');
-INSERT OR IGNORE INTO Modifiers (ModifierId , ModifierType , SubjectRequirementSetId , OwnerRequirementSetId) VALUES
-	('BBG_SPAIN_BUILDING_PROD_BOOST' , 'MODIFIER_PLAYER_CITIES_ADJUST_BUILDING_PRODUCTION' , NULL , NULL);
-INSERT OR IGNORE INTO ModifierArguments (ModifierId , Name , Value) VALUES
-	('BBG_SPAIN_BUILDING_PROD_BOOST' , 'DistrictType' , 'DISTRICT_HARBOR'),
-	('BBG_SPAIN_BUILDING_PROD_BOOST' , 'Amount'       , '25');
-
 -- Leader is Phillipe Requirement.
 INSERT OR IGNORE INTO RequirementSets(RequirementSetId , RequirementSetType) VALUES
 	('BBG_PLAYER_IS_SPAIN', 'REQUIREMENTSET_TEST_ANY');
